c5f063ab33725d9f5f9621f1684f7c35ad606feb
Sinan URUN <sinanurun24@gmail.com>
2021-01-12 15:45:42 +0300

    yeni dersler

---
+++ listeler/ders09_icice_liste.py
@@ -0,0 +1,13 @@
+bilisim = []
+dokuz_a = ["Ege","Ali"]
+dokuz_b = ["Muhammed","Eren","Recep","Yusuf"]
+dokuz_c = ["Erkan","Can","Orhan"]
+print(bilisim)
+bilisim.append(dokuz_a)
+print(bilisim)
+bilisim.append(dokuz_b)
+print(bilisim)
+bilisim.append(dokuz_c)
+print(bilisim)
+print(bilisim[0][1])
+print(bilisim[2][2])
